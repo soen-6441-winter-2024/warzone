e3c53f585a1611fbc338f2f6c4f4a3c4dfde4ce9
jldiaz <jdiaz216@gmail.com>
2024-03-10 10:15:58 -0400

    Added abstract repository to refactor country repository

--- src/main/java/ca/concordia/app/warzone/repository/impl/CountryRepositoryMemoryImpl.java
+++ src/main/java/ca/concordia/app/warzone/repository/impl/CountryRepositoryMemoryImpl.java
@@ -1,15 +1,19 @@
 package ca.concordia.app.warzone.repository.impl;
-import ca.concordia.app.warzone.repository.CountryRepository;
+
 import ca.concordia.app.warzone.model.Country;
+import ca.concordia.app.warzone.repository.CountryRepository;
 import org.springframework.stereotype.Repository;
-import java.util.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 /**
  * Implementation of the CountryRepository interface using an in-memory data structure.
  */
 @Repository
-public class CountryRepositoryMemoryImpl implements CountryRepository {
+public class CountryRepositoryMemoryImpl extends AbstractRepositoryMemoryImpl<Country> implements CountryRepository{
 
     /**
      * Default constructor
@@ -18,43 +22,12 @@ public class CountryRepositoryMemoryImpl implements CountryRepository {
     private Map<String, Country> d_countries = new HashMap<>(); // Stores countries with their IDs as keys
 
     /**
-     * Saves a country to the repository.
-     *
-     * @param p_country the country to be saved
-     */
-    @Override
-    public void save(Country p_country) {
-        d_countries.put(p_country.getId(), p_country);
-    }
-
-    /**
-     * Retrieves a country by its ID.
-     *
-     * @param p_id the ID of the country to retrieve
-     * @return an Optional containing the country, or empty if not found
-     */
-    @Override
-    public Optional<Country> findById(String p_id) {
-        return Optional.ofNullable(d_countries.get(p_id));
-    }
-
-    /**
      * Returns all countries that match the continent id
      * @param p_continentId continentId
      * @return a list of countries
      */
     public List<Country> findByContinentId(String p_continentId) {
             return this.d_countries.values().stream().filter(c -> c.getContinent().getId().equals(p_continentId)).collect(Collectors.toList());
-    }
-
-    /**
-     * Deletes a country by its ID.
-     *
-     * @param p_id the ID of the country to delete
-     */
-    @Override
-    public void deleteById(String p_id) {
-        d_countries.remove(p_id);
     }
 
     /**
@@ -67,13 +40,8 @@ public class CountryRepositoryMemoryImpl implements CountryRepository {
         d_countries.put(p_country.getId(), p_country);
     }
 
-    /**
-     * Retrieves all countries stored in the repository.
-     *
-     * @return a list of all countries
-     */
     @Override
-    public List<Country> findAll() {
-        return new ArrayList<>(d_countries.values());
+    Map<String, Country> getMap() {
+        return d_countries;
     }
 }
