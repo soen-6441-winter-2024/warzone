df461f3d06119cb8496ab5811b56fb1f5cd21e56
jldiaz <jdiaz216@gmail.com>
2024-03-17 20:45:39 -0400

    refactored validate map structure

--- src/test/java/ca/concordia/app/warzone/service/MapServiceTest.java
+++ src/test/java/ca/concordia/app/warzone/service/MapServiceTest.java
@@ -1,35 +1,30 @@
 package ca.concordia.app.warzone.service;
 
-import ca.concordia.app.warzone.repository.ContinentRepository;
-import ca.concordia.app.warzone.repository.CountryRepository;
-import ca.concordia.app.warzone.repository.MapRepository;
-import org.junit.jupiter.api.BeforeEach;
+import ca.concordia.app.warzone.console.ConsoleRunner;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@SpringBootTest
 @ExtendWith(MockitoExtension.class)
 public class MapServiceTest {
 
     public static final String VALID_FILE_NAME = "src/test/resources/valid_map.txt";
+    public static final String VALID_FILE_NAME_EMPTY_BORDERS = "src/test/resources/valid_map_empty_section.txt";
+    public static final String INVALID_FILE_NAME_EMPTY_COUNTRIES = "src/test/resources/invalid_map_empty_countries.txt";
     public static final String INVALID_FILE_NAME = "src/test/resources/invalid_map.txt";
 
-    @Mock
-    private MapRepository mapRepository;
-    @Mock
-    private CountryRepository countryRepository;
-    @Mock
-    private ContinentRepository continentRepository;
-    private MapService underTest;
+    @MockBean
+    private ConsoleRunner consoleRunner;
 
-    @BeforeEach
-    public void before() {
-        underTest = new MapService(mapRepository, countryRepository, continentRepository);
-    }
+    @Autowired
+    private MapService underTest;
 
     @Test
     public void testValidateMapStructure() {
@@ -37,6 +32,22 @@ public class MapServiceTest {
         boolean result = underTest.validateMapStructure(VALID_FILE_NAME);
 
         assertTrue(result);
+    }
+
+    @Test
+    public void testValidateMapStructureEmptyBorders() {
+
+        boolean result = underTest.validateMapStructure(VALID_FILE_NAME_EMPTY_BORDERS);
+
+        assertFalse(result);
+    }
+
+    @Test
+    public void testValidateMapStructureEmptyCountries() {
+
+        boolean result = underTest.validateMapStructure(INVALID_FILE_NAME_EMPTY_COUNTRIES);
+
+        assertFalse(result);
     }
 
     @Test
