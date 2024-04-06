cd1a94af49d1bf31c413b1fa0311c691626dafa9
mosope.dev <mosope.adeyemi.dev@gmail.com>
2024-04-06 17:03:41 -0400

    fix advance order to update country owners

--- src/main/java/ca/concordia/app/warzone/model/Player.java
+++ src/main/java/ca/concordia/app/warzone/model/Player.java
@@ -118,12 +118,32 @@ public class Player implements Entity {
     }
 
     /**
+     * Removes a country assigned to a player if they lose the country
+     * @param countryId the id of the country to remove
+     */
+    public void removeAssignedCountry(String countryId) {
+        for (int i = 0; i < d_countriesAssigned.size(); i++) {
+            if(d_countriesAssigned.get(i).equals(countryId)) {
+                d_countriesAssigned.remove(i);
+            }
+        }
+    }
+
+    /**
      * Sets the list of countries assigned to the player.
      *
      * @param p_countriesAssigned the list of countries assigned to the player
      */
     public void setCountriesAssigned(List<Country> p_countriesAssigned){
         this.d_countriesAssigned = p_countriesAssigned;
+    }
+
+    /**
+     * Adds a country to list of countries owned by player
+     * @param p_newlyConqueredCountry the country to add.
+     */
+    public void addNewConqueredCountry(Country p_newlyConqueredCountry) {
+        this.d_countriesAssigned.add(p_newlyConqueredCountry);
     }
 
     /**
