c67d742082f87e16c99911c8aedd1563dbd19886
Denys Rosario <denys458@gmail.com>
2024-04-09 22:06:35 -0400

    feat: implement tournament command

--- src/main/java/ca/concordia/app/warzone/console/commands/implementations/TournamentCommand.java
+++ src/main/java/ca/concordia/app/warzone/console/commands/implementations/TournamentCommand.java
@@ -5,6 +5,11 @@ import ca.concordia.app.warzone.console.exceptions.InvalidCommandException;
 import ca.concordia.app.warzone.controller.GameEngineController;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Represents the 'tournament' command.
  * This command is used to initiate a tournament with specified parameters.
@@ -32,34 +37,87 @@ public class TournamentCommand extends Command {
      */
     @Override
     public String run(String[] p_subCommandsAndOptions) {
-        if(p_subCommandsAndOptions.length != 8) {
-            throw new InvalidCommandException("expected 8 arguments");
-        }
-
-        String aM = p_subCommandsAndOptions[0];
-        String aListMapFiles = p_subCommandsAndOptions[1];
-        String aP = p_subCommandsAndOptions[2];
-        String aListPlayerStrategies = p_subCommandsAndOptions[3];
-        String aG = p_subCommandsAndOptions[4];
-        String aNumberGamesStr = p_subCommandsAndOptions[5];
-        String aD = p_subCommandsAndOptions[6];
-        String aMaxNumberTurnsStr = p_subCommandsAndOptions[7];
+        String[] mapFilenames = this.getMapFilenames(p_subCommandsAndOptions);
+        String[] playerStrategies = this.getPlayerStrategies(p_subCommandsAndOptions);
+        int maxTurns = this.getMaxTurns(p_subCommandsAndOptions);
+        int gamesAmount = this.getGamesAmount(p_subCommandsAndOptions);
 
 
-        int numberGames = 0;
-        try {
-            numberGames = Integer.parseInt(aNumberGamesStr);
-        } catch (NumberFormatException e) {
-            throw new InvalidCommandException("number of games should be a number");
-        }
-        int maxNumberTurns = 0;
-        try {
-            maxNumberTurns = Integer.parseInt(aMaxNumberTurnsStr);
-        } catch (NumberFormatException e) {
-            throw new InvalidCommandException("Maximum number of turns should be a number");
-        }
 
-        //return d_gameEngineController.advance(countryFrom, countryTo, armiesQuantity);
         return "";
+    }
+
+    private int getMaxTurns(String[] p_subCommandsAndOptions) {
+        Pattern pattern = Pattern.compile("(?<=-D) ((((\\S+))\\s)+)");
+        Matcher matcher = pattern.matcher(String.join(" ", p_subCommandsAndOptions));
+
+        if(!matcher.find()) {
+            throw new InvalidCommandException("must specify a max turn number");
+        }
+
+        String gameAmountStr = matcher.group().trim();
+        try {
+            int maxTurnNumber = Integer.parseInt(gameAmountStr);
+            if(maxTurnNumber < 10 || maxTurnNumber > 50) {
+                throw new InvalidCommandException("max turn number must be between 10 and 50");
+            }
+
+            return maxTurnNumber;
+        } catch (NumberFormatException e){
+            throw new InvalidCommandException("invalid max number of turns passed");
+        }
+    }
+
+    private int getGamesAmount(String[] p_subCommandsAndOptions) {
+        Pattern pattern = Pattern.compile("(?<=-G) ((((\\S+))\\s)+)(?=-D)");
+        Matcher matcher = pattern.matcher(String.join(" ", p_subCommandsAndOptions));
+
+        if(!matcher.find()) {
+            throw new InvalidCommandException("must specify a game amount");
+        }
+
+        String gameAmountStr = matcher.group().trim();
+        try {
+            int gameAmount = Integer.parseInt(gameAmountStr);
+            if(gameAmount < 1 || gameAmount > 5) {
+                throw new InvalidCommandException("game amount number must be between 1 and 5");
+            }
+
+            return gameAmount;
+        } catch (NumberFormatException e){
+            throw new InvalidCommandException("invalid game amount passed");
+        }
+    }
+
+    private String[] getPlayerStrategies(String[] p_subCommandsAndOptions) {
+        Pattern pattern = Pattern.compile("(?<=-P) ((((\\S+))\\s)+)(?=-G)");
+        Matcher matcher = pattern.matcher(String.join(" ", p_subCommandsAndOptions));
+
+        if(!matcher.find()) {
+            throw new InvalidCommandException("must specify at least 2 player strategies");
+        }
+
+        String[] strategies = matcher.group().trim().split(" ");
+        if(strategies.length > 4) {
+            throw new InvalidCommandException("strategies quantity must be between 2 and 4");
+        }
+
+        return strategies;
+    }
+
+    private String[] getMapFilenames(String[] p_subCommandsAndOptions) {
+        Pattern mapFilesPattern = Pattern.compile("(?<=-M) ((((\\S+))\\s)+)(?=-P)");
+        Matcher matcher = mapFilesPattern.matcher(String.join(" ", p_subCommandsAndOptions));
+
+        if(!matcher.find()) {
+            throw new InvalidCommandException("must specify file names");
+        }
+
+        String[] mapFileNames = matcher.group().trim().split(" ");
+        if(mapFileNames.length > 5) {
+            throw new InvalidCommandException("map files quantity must be between 1 and 5");
+        }
+
+        return mapFileNames;
     }
 }
