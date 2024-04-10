aad20fd097c80253f8ee682022db9db6a0890aa6
jldiaz <jdiaz216@gmail.com>
2024-04-10 02:58:05 -0400

    Added missing javadocs

---
+++ src/main/java/ca/concordia/app/warzone/model/MapFileFormat.java
@@ -0,0 +1,17 @@
+package ca.concordia.app.warzone.model;
+
+/**
+ * Enum that represents the different supported map formats.
+ */
+public enum MapFileFormat {
+
+    /**
+     * Default format
+     */
+    DEFAULT,
+
+    /**
+     * Conquest format
+     */
+    CONQUEST
+}
